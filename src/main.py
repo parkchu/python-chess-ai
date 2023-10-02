5c1399027965187009b9db4914f4756bbdd10351
parkchu <parkjoohan0603@gmail.com>
2023-10-02 04:47:58 +0000

    feat: create Pawn

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from pieces.Pawn import Pawn
+
+def run():
+    pawn = Pawn("white")
+    print(pawn.team)
+
+run()
---
+++ src/pieces/Pawn.py
@@ -0,0 +1,4 @@
+class Pawn:
+    def __init__(self, team):
+        self.team = team
+        
